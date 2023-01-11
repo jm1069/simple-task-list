2f1c858bac3bb1ec85b5aac5f45feec40001f2f1
Jonas <78270099+jm1069@users.noreply.github.com>
2023-01-11 10:39:58 +0100

    indentation

--- app.js
+++ app.js
@@ -19,19 +19,19 @@ form.addEventListener("submit", (e) => {
 
 // Add a new task to the list
 function addTask(task) {
-  const newTask = document.createElement("li");
-  newTask.classList.add("task");
-  newTask.innerHTML = task;
+    const newTask = document.createElement("li");
+    newTask.classList.add("task");
+    newTask.innerHTML = task;
 
-  const deleteBtn = document.createElement("button");
-  deleteBtn.innerHTML = '<i class="fa-solid fa-trash"></i>';
-  deleteBtn.classList.add("delete-btn");
-  newTask.appendChild(deleteBtn);
-  
-  taskList.appendChild(newTask);
-  
-  // Add a click event to delete a task
-  deleteBtn.addEventListener("click", (e) => {
-    taskList.removeChild(newTask);
-  });
+    const deleteBtn = document.createElement("button");
+    deleteBtn.innerHTML = '<i class="fa-solid fa-trash"></i>';
+    deleteBtn.classList.add("delete-btn");
+    newTask.appendChild(deleteBtn);
+    
+    taskList.appendChild(newTask);
+    
+    // Add a click event to delete a task
+    deleteBtn.addEventListener("click", (e) => {
+        taskList.removeChild(newTask);
+    });
 }
